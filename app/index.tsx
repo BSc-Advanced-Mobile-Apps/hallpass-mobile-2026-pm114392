78fc09c33dab895d122484e8c1348bc96ca8d7b1
PM114392 <PM114392@hc.ac.je>
2026-03-19 09:33:29 +0000

    Updated index matched with Ged's

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Text } from '@/components/ui/text';
+import { Checkbox } from '@/components/ui/checkbox';
 
 export default function HomeScreen() {
   return (
     <View className="flex flex-1 py-32 bg-background">
+      <View className='flex flex-row'>
+        <View className='flex w-16 items-center justify-center'>
+          <Checkbox checked={false} className='border-2'/>
+          </View>
+          <View className='border-foreground-transparent border-b py-4'>
       <Text className="text-foreground">Hello, world!</Text>
       <Text className="text-foreground-transparent">Date, May 2019</Text>
+      </View>
+    
+    </View>
     </View>
   );
 }
